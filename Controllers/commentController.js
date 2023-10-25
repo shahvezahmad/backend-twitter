57974995fb872701e333bf2d121618af61fc8bac
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-10-25 23:33:03 +0530

    create, delete, edit and populate comment

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -2,6 +2,7 @@ const Comment = require("../Models/commentModel");
 const Tweet = require("../Models/tweetModel");
 const User = require("../Models/userModel");
 const moment = require("moment");
+const path = require("path");
 
 //create comment
 const createComment = (req, res) => {
@@ -58,7 +59,46 @@ const deleteComment = (req, res) => {
     );
 };
 
+//edit comment
+const editComment = (req, res) => {
+  Comment.findOne({ postedCommentTime: req.params.commentId }, (err, doc) => {
+    doc.content = req.body.content;
+    doc.isEdited = true;
+    doc.save();
+    return res.json({
+      status: "ok",
+    });
+  });
+};
+
+//populate comments of a particular tweet
+const populateComment = async (req, res) => {
+  try {
+    const tweet = await Tweet.findOne({ postedTweetTime: req.params.tweetId })
+      .populate("postedBy")
+      .populate({
+        path: "comments",
+        populate: [{ path: "postedBy" }],
+      })
+      .exec();
+
+    if (tweet) {
+      return res.json({
+        status: "ok",
+        tweet: tweet,
+      });
+    } else {
+      return res.json({ status: "error", error: "comments not found" });
+    }
+  } catch (error) {
+    return res.json({ status: "error", error: "An error occurred" });
+  }
+};
+
+
 module.exports = {
     createComment,
-    deleteComment
+    deleteComment,
+    editComment,
+    populateComment
 }
