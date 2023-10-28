cd3048149943159af368e6deb96b3df3e4a8b1a5
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-10-28 22:23:51 +0530

    get feed, refactored feed controller

--- Controllers/feedController.js
+++ Controllers/feedController.js
@@ -1,40 +1,61 @@
-const moment = require("moment");
 const Tweet = require("../Models/tweetModel");
 const User = require("../Models/userModel");
+const jwt = require("jsonwebtoken");
 
-const feed = async (req, res) => {
-    const info = req.body;
-    const tweetInfo = JSON.parse(req.body.tweet);
-  
-    newTweet = Tweet.create(
-      {
-        content: tweetInfo.content,
-        retweets: [],
-        postedTweetTime: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      },
-      (err, newTweet) => {
-        if (!err) {
-          if (info.image) {
-            newTweet.image = info.image;
-          } else console.log("no image found");
-          User.findOne({ username: tweetInfo.postedBy.username }, (err, doc) => {
-            if (!err) {
-              newTweet.postedBy = doc._id;
-              if (newTweet.postedBy) {
-                newTweet.save();
-                doc.tweets.unshift(newTweet._id);
-                doc.save();
-                return res.json({ image: info.image });
-              } else
-                return res.json({ status: "error", error: "An error occured" });
-            } else
-              return res.json({ status: "error", error: "An error occured" });
-          });
+// feed
+const getFeed = async (req, res) => {
+  const token = req.headers["x-access-token"];
+  const tweetsToSkip = req.query.t || 0;
+
+  try {
+    const decoded = jwt.verify(token, "newSecretKey");
+    const username = decoded.username;
+    const user = await User.findOne({ username: username });
+
+    const docs = await Tweet.find({ isRetweeted: false })
+      .populate("postedBy")
+      .populate("comments")
+      .sort({ createdAt: -1 })
+      .skip(tweetsToSkip)
+      .limit(20)
+      .exec();
+
+    // Process the documents, set properties, etc.
+    for (const doc of docs) {
+      // Set properties based on conditions
+      if (!doc.likes.includes(username)) {
+        doc.likeTweetBtn = "black";
+      } else {
+        doc.likeTweetBtn = "deeppink";
+      }
+
+      // Set properties for comments
+      for (const docComment of doc.comments) {
+        if (!docComment.likes.includes(username)) {
+          docComment.likeCommentBtn = "black";
+        } else {
+          docComment.likeCommentBtn = "deeppink";
         }
       }
-    );
+
+      // Set properties for retweets
+      if (!doc.retweets.includes(username)) {
+        doc.retweetBtn = "black";
+      } else {
+        doc.retweetBtn = "green";
+      }
+    }
+
+    return res.json({
+      status: "ok",
+      tweets: docs,
+      activeUser: user,
+    });
+  } catch (error) {
+    return res.json({ status: "error", error: "Session ended :(" });
+  }
 };
 
 module.exports = {
-    feed
-}
+  getFeed
+};
